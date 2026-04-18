73afbe43bcbc6ea75b1ec6c7b5908eb4aa687ae4
shirena857-hub <shirena857@gmail.com>
2026-04-18 15:01:34 +0600

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,44 @@
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#,                         ︵
+#                        /'_/) 
+#                      /¯ ../ 
+#                    /'..../ 
+#                  /¯ ../ 
+#                /... ./
+#   ¸•´¯/´¯ /' ...'/´¯`•¸  
+# /'.../... /.... /.... /¯\
+#('  (...´.(,.. ..(...../',    \
+# \'.............. .......\'.    )      
+#   \'....................._.•´/
+#     \ ....................  /
+#       \ .................. |
+#         \  ............... |
+#           \............... |
+#             \ .............|
+#               \............|
+#                 \ .........|
+#                   \ .......|
+#                     \ .....|
+#                       \ ...|
+#                         \ .|
+#                           \\
+#                             \('-') 
+#   ,,                           |_|\
+#                               | |
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED
+
+
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED
+#DONT CHANGE CREDIT 
+#IF YOU CHANGE MY CREDIT, I'LL FUCK YOUR MOM
